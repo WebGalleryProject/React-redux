9c2c9422a105e3bbcb57e18c169d674a76e4e3aa
Sergey Menyailo <Smenyailo@bk.ru>
2021-04-16 15:05:10 +0300

    Удаление, завершение и пометка задач как выжные Кнопка добавления новой задачи (пока заглушка) Реализовано отображение количества задач

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
-const TodoListItem = ({ label, important = false }) => {
-
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+export default class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
   };
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+  render() {
+    const { label, onDeleted, onToggleImportant, onToggleDone, important, done } = this.props;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
+
+    return (
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={onToggleDone}>
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" onClick={onToggleImportant} />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" onClick={onDeleted} />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
